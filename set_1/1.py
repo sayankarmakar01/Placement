f635fe12f238ba88f658c747e250aba85dc1e10e
Sayan Karmakar <89528429+sayankarmakar01@users.noreply.github.com>
2025-07-11 23:45:43 +0530

    Create 1.py

---
+++ set_1/1.py
@@ -0,0 +1,6 @@
+l=int(input("Enter Length: "))
+b=int(input("Enter Breadth: "))
+
+area=l*b
+perimeter=2*(l+b)
+print("The Area of Rectangle is: ",area,"\nThe Perimeter of Rectangle is: ",perimeter)
