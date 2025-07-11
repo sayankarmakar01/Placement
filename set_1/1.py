7d83320db5f482b22aaa63ed08d1f5996766c595
Sayan Karmakar <89528429+sayankarmakar01@users.noreply.github.com>
2025-07-12 00:52:39 +0530

    modification 1

--- set_1/1.py
+++ set_1/1.py
@@ -1,6 +1,8 @@
+import math
 l=int(input("Enter Length: "))
 b=int(input("Enter Breadth: "))
 
 area=l*b
 perimeter=2*(l+b)
-print("The Area of Rectangle is: ",area,"\nThe Perimeter of Rectangle is: ",perimeter)
+diagonal=math.sqrt(l**2+b**2)
+print("The Area of Rectangle is: ",area,"\nThe Perimeter of Rectangle is: ",perimeter,"\nThe Diagonal of Rectangle is: ",diagonal)
