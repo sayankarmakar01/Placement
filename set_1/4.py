d5390d8929543a6251ca4c81f91cba751e3b8988
Sayan Karmakar <89528429+sayankarmakar01@users.noreply.github.com>
2025-07-12 00:45:53 +0530

    update

---
+++ set_1/4.py
@@ -0,0 +1,4 @@
+import math
+r=int(input("Enter the Radius: "))
+area=4*math.pi*r**2
+print("Area of the Sphere is: ",area)
---
+++ set_1/5.py
@@ -0,0 +1,2 @@
+l=int(input("Enter the lenght of a side: "))
+print("The volume of the cube is: ",l**3)
