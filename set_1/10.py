4c4a65722dd3071d038460f7f769c88e7019b625
Sayan Karmakar <89528429+sayankarmakar01@users.noreply.github.com>
2025-07-21 14:41:58 +0530

    update

---
+++ set_1/10.py
@@ -0,0 +1,7 @@
+l=[]
+for i in range(0,10):
+    l.append(int(input("Enter number ")))
+max_n=max(l)
+min_n=min(l)
+
+print("Smallest Number is: ",min_n," and Largest number is: ",max_n)
--- set_1/9.py
+++ set_1/9.py
@@ -1,1 +1,10 @@
-print("Hello")
+n=int(input("Enter the number: "))
+
+sum=0
+for i in range(1,n):
+    if n%i == 0:
+        sum+=i
+if n==sum:
+    print(n," is a perfect number")
+else:
+    print(n," is not a perfect number")
